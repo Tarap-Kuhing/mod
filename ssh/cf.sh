49717c43fbd0856d7f9146edc99d64bc6b031e7d
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-02 16:53:10 +0700

    Add files via upload

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -1,46 +1,20 @@
 #!/bin/bash
-MYIP=$(wget -qO- icanhazip.com);
-apt install jq curl -y
-
-DOMAIN=tarong.my.id
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-#subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.tarong.my.id
-#NS_dns=${subsl}.tarong.my.id
-CF_ID=merahjambo@gmail.com
-CF_KEY=86431de017f7bf317c3960061da2f87c8effb
-set -euo pipefail
-IP=$(wget -qO- icanhazip.com);
-echo "Updating DNS for ${dns}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
-#echo "$dns" > /root/scdomain
-echo $dns > /root/domain
-#echo "$dns" > /etc/xray/scdomain
-echo "$dns" > /etc/xray/domain
-echo "$dns" > /etc/v2ray/domain
-#echo $dns > /root/domain
-echo "IP=$dns" > /var/lib/ipvps.conf
-echo "IP=$dns" > /var/lib/Tarap-Kuhing/ipvps.conf
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" > /var/lib/Tarap-Kuhing/ipvps.conf
+rm -rf /etc/xray/domain
+echo $domain > /etc/xray/domain
 cd
