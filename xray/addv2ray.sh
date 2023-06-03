e3c39df6a69dd0ac9ffdb4e2fb2de4d39e63057b
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-03 22:11:42 +0700

    Add files via upload

--- xray/addv2ray.sh
+++ xray/addv2ray.sh
@@ -96,8 +96,7 @@ TEXT="
 <code>${xrayv2ray2}</code>
 <code>───────────────────────────</code>
 <code>Expired On : $hariini</code>
-<code>Expired On : $exp</code>
-"
+<code>Expired On : $exp</code>"
 rm -rf /etc/xray/vmess-$user-tls.json
 rm -rf /etc/xray/vmess-$user-nontls.json
 systemctl restart xray.service
@@ -105,21 +104,21 @@ systemctl reload nginx
 service cron restart
 clear
 echo -e ""
-echo -e "======-XRAYS/VMESS-======" | tee -a /etc/xray/$user.log
-echo -e "Remarks     : ${user}" | tee -a /etc/xray/$user.log
-echo -e "IP/Host     : ${MYIP}" | tee -a /etc/xray/$user.log
-echo -e "Address     : ${domain}" | tee -a /etc/xray/$user.log
-echo -e "Port TLS    : ${tls}" | tee -a /etc/xray/$user.log
-echo -e "Port No TLS : ${nontls}" | tee -a /etc/xray/$user.log
-echo -e "User ID     : ${uuid}" | tee -a /etc/xray/$user.log
-echo -e "Alter ID    : 0" | tee -a /etc/xray/$user.log
-echo -e "Security    : auto" | tee -a /etc/xray/$user.log
-echo -e "Network     : ws" | tee -a /etc/xray/$user.log
-echo -e "Path        : /vmess" | tee -a /etc/xray/$user.log
-echo -e "Created     : $hariini" | tee -a /etc/xray/$user.log
-echo -e "Expired     : $exp" | tee -a /etc/xray/$user.log
-echo -e "=========================" | tee -a /etc/xray/$user.log
-echo -e "Link TLS    : ${xrayv2ray1}" | tee -a /etc/xray/$user.log
-echo -e "=========================" | tee -a /etc/xray/$user.log
-echo -e "Link No TLS : ${xrayv2ray2}" | tee -a /etc/xray/$user.log
-echo -e "=========================" | tee -a /etc/xray/$user.log
+echo -e "======-XRAYS/VMESS-======" | tee -a /etc/xray/vmess/$user.log
+echo -e "Remarks     : ${user}" | tee -a /etc/xray/vmess/$user.log
+echo -e "IP/Host     : ${MYIP}" | tee -a /etc/xray/vmess/$user.log
+echo -e "Address     : ${domain}" | tee -a /etc/xray/vmess/$user.log
+echo -e "Port TLS    : ${tls}" | tee -a /etc/xray/vmess/$user.log
+echo -e "Port No TLS : ${nontls}" | tee -a /etc/xray/vmess/$user.log
+echo -e "User ID     : ${uuid}" | tee -a /etc/xray/vmess/$user.log
+echo -e "Alter ID    : 0" | tee -a /etc/xray/vmess/$user.log
+echo -e "Security    : auto" | tee -a /etc/xray/vmess/$user.log
+echo -e "Network     : ws" | tee -a /etc/xray/vmess/$user.log
+echo -e "Path        : /vmess" | tee -a /etc/xray/vmess/$user.log
+echo -e "Created     : $hariini" | tee -a /etc/xray/vmess/$user.log
+echo -e "Expired     : $exp" | tee -a /etc/xray/vmess/$user.log
+echo -e "=========================" | tee -a /etc/xray/vmess/$user.log
+echo -e "Link TLS    : ${xrayv2ray1}" | tee -a /etc/xray/vmess/$user.log
+echo -e "=========================" | tee -a /etc/xray/vmess/$user.log
+echo -e "Link No TLS : ${xrayv2ray2}" | tee -a /etc/xray/vmess/$user.log
+echo -e "=========================" | tee -a /etc/xray/vmess/$user.log
--- xray/listvmess.sh
+++ xray/listvmess.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 clear
+tarap=$(cat /etc/xray/vmess/)
+echo "$tarap"
+echo""
+echo""
 read -rp "Masukan Name User : " user
-list=$(cat /etc/xray/$user.log)
+list=$(cat /etc/xray/vmess/$user.log)
 clear
 echo "$list"
