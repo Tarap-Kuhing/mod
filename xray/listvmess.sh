f08257baea3599451851b4c65a62cb8dadafaa55
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-03 23:43:16 +0800

    Update listvmess.sh

--- xray/listvmess.sh
+++ xray/listvmess.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 clear
-tarap=$(cat /etc/xray/vmess)
-echo "$tarap"
-echo""
-echo""
-read -rp "Masukan Name User : " user
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmess")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/etc/xray/vmess" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " user
+		fi
+	done
+#tarap=$(cat /etc/xray/vmess)
+#echo "$tarap"
+#echo""
+#echo""
+#read -rp "Masukan Name User : " user
 list=$(cat /etc/xray/vmess/$user.log)
 clear
 echo "$list"
