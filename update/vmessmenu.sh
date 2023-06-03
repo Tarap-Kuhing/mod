91c6a42941df491a888764d9ef95a75dd847e87f
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-03 19:52:16 +0700

    Add files via upload

--- update/vmessmenu.sh
+++ update/vmessmenu.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
+# //====================================================
+# //	System Request:Debian 9+/Ubuntu 18.04+/20+
+# //	Author:	Tarap-Kuhing
+# //	Dscription: Xray Menu Management
+# //	email: merahjambo@gmail.com
+# //  telegram: https://t.me/Baung2012
+# //====================================================
+# // font color configuration | T A R A P  K U H I N G AUTOSCRIPT
 clear
-m="\033[0;1;36m"
-y="\033[0;1;37m"
-yy="\033[0;1;32m"
-yl="\033[0;1;33m"
-wh="\033[0m"
-echo -e "$y                             VMESS $wh"
-echo -e "$y-------------------------------------------------------------$wh"
-echo -e "$yy 1$y. Create Account XRAYS Vmess Websocket"
-echo -e "$yy 2$y. Delete Account XRAYS Vmess Websocket"
-echo -e "$yy 3$y. Extending Account XRAYS Vmess Active Life"
-echo -e "$yy 4$y. Check User Login XRAYS Vmess"
-echo -e "$yy 5$y. Renew Certificate XRAYS Account"
-echo -e "$yy 6$y. Menu"
-echo -e "$yy 7$y. Exit"
-echo -e "$y-------------------------------------------------------------$wh"
-read -p "Select From Options [ 1 - 7 ] : " menu
+grenbo="\e[92;1m"
+NC='\033[0m'
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          MENU MANAGER VMESS              $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mCreating a Vmess Account${NC}"
+echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mDelete Vmess Account${NC}"
+echo -e "\033[1;93m│  ${grenbo}3.${NC} \033[0;36mRenew Vmess Account${NC}"
+echo -e "\033[1;93m│  ${grenbo}4.${NC} \033[0;36mCheck Vmess login Account${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
+read -p "Select From Options [ 1 - 4 ] : " menu
 case $menu in
 1)
-addvmess
-;;
+    addvmess
+    ;;
 2)
-delvmess
-;;
+    delvmess
+    ;;
 3)
-renewvmess
-;;
+    renewvmess
+    ;;
 4)
-cekvmess
-;;
-5)
-certv2ray
-;;
-6)
-clear
-menu
-;;
-7)
-clear
-exit
-;;
+    cekvmess
+    ;;
 *)
-clear
-menu
-;;
+    menu
+    ;;
 esac
