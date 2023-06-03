f5a796a5e38a8ba052bcd5920670bfb06b581ccd
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-04 00:07:04 +0800

    Update listvmess.sh

--- xray/listvmess.sh
+++ xray/listvmess.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-clear
-	echo "Select the existing client you want to renew"
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmes-tls.json")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing name client you want to list"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^### " "/etc/xray/vmess" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/xray/vmess-tls.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
