8aefafbc7179bcb9b0f9d20498bb89657cb1757d
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-04 00:12:52 +0800

    Update listvmess.sh

--- xray/listvmess.sh
+++ xray/listvmess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmes-tls.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmess-tls.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -13,18 +13,11 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/vmes-tls.json")
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
 	grep -E "^### " "/etc/xray/vmess-tls.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " user
-		fi
-	done
 #tarap=$(cat /etc/xray/vmess)
 #echo "$tarap"
 #echo""
 #echo""
-#read -rp "Masukan Name User : " user
+read -rp "Masukan Name User : " user
 list=$(cat /etc/xray/vmess/$user.log)
 clear
 echo "$list"
