48c09911b62e6bae54ad91acbf900d00c99e73f5
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-04 08:26:59 +0800

    Update listvmess.sh

--- xray/listvmess.sh
+++ xray/listvmess.sh
@@ -10,10 +10,11 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 
 	clear
 	echo ""
-	echo "Masukan Nama User Kamu"
+	echo "List Name User Kamu"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
 	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	echo ""
 	read -rp "Masukan Name User : " user
 echo ""
 echo ""
